cfef356904c9a93f864b4b9d8f3cb4b18314723b
caleb22cottrell <caleb22cottrell@gmail.com>
2026-04-14 08:17:41 -0400

    update

--- js/script.js
+++ js/script.js
@@ -1,15 +1,4 @@
 console.log("script.js connected!");
 
 
-// RSVP button interaction
-document.addEventListener("DOMContentLoaded", () => {
-  const buttons = document.querySelectorAll(".rsvp-btn");
-
-  buttons.forEach(btn => {
-    btn.addEventListener("click", () => {
-      alert("You have RSVP'd!");
-    });
-  });
-});
-
 // API for dining options, events, and map locations would go here. For now, we can use placeholders.
